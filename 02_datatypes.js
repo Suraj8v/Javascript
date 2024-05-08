8cc5e07d9aa991fd1a9bedcb96dcb5a1db924c56
Suraj Jagdale <suraj.jagdale@expleogroup.com>
2024-05-08 14:46:32 +0530

    committing

---
+++ 02_datatypes.js
@@ -0,0 +1,15 @@
+let var1 = "fifty"
+
+let var2 = Number(var1)
+
+let numString = "34"
+
+let var3 = Number(numString)
+
+console.log(var2)
+
+console.log(var1)
+
+console.log(typeof var2)
+
+console.log("var3 : "+var3)

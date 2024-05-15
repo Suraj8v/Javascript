10fc73aeac83bc015ea038e43cdbe25164548a8e
Suraj Jagdale <suraj.jagdale@expleogroup.com>
2024-05-15 18:45:48 +0530

    commiting

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -1,19 +1,35 @@
-// Strings are text, written within double or single quotes:
-//JavaScript identifiers are case-sensitive.
-//Since JavaScript treats a dollar sign as a letter, identifiers containing $ are valid variable names
+// // Strings are text, written within double or single quotes:
+// //JavaScript identifiers are case-sensitive.
+// //Since JavaScript treats a dollar sign as a letter, identifiers containing $ are valid variable names j
 
-let var1 = "fifty"
+// let var1 = "fifty"
 
-let var2 = Number(var1)
+// let var2 = Number(var1)
 
-let numString = "34"
+// let numString = "34"
 
-let var3 = Number(numString)
+// let var3 = Number(numString)
 
-console.log(var2)
+// console.log(var2)
 
-console.log(var1)
+// console.log(var1)
 
-console.log(typeof var2)
+// console.log(typeof var2)
 
-console.log("var3 : "+var3)
+// console.log("var3 : "+var3)
+
+// console.log(1+2+"3") 
+// console.log("1"+2+3) 
+
+
+console.log("start")
+
+ setTimeout(()=>{
+    console.log("settimeout");
+},10)
+
+
+
+for(var i = 0 ; i<1000;i++){
+    console.log("for"+i);
+}
